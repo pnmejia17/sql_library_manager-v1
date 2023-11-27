7ebe2250c461b9c13b98d48131c1321cc9f397dc
pnmejia17 <114623502+pnmejia17@users.noreply.github.com>
2023-11-27 09:44:53 -0500

    bug posting new book

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
+const Book = require('../models').Book
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.redirect('/books');
+})
+
+
+// Get Books 
+
+router.get('/books', async function (req, res, next) {
+  const books = await Book.findAll();
+  console.log(books);
+  res.render('index', {
+    books: books,
+    title: 'Books'
+  });
+
+})
+
+router.get('/books/new', async function (req, res, next) {
+  res.render('new-book', {
+    book: {}
+  });
+})
+
+router.post('books/new', async function (req, res, next){
+  await Book.create(req.body)
+  res.redirect('/books')
+})
 
 module.exports = router;
