ba197a2599039371949a27ea68032c314a94169b
pnmejia17 <114623502+pnmejia17@users.noreply.github.com>
2023-11-28 08:51:37 -0500

    Revert "attempted to fix bugs"
    This reverts commit 1e0939949687ab2aea0e5ee633be13b3fb268b70.

--- routes/books.js
+++ routes/books.js
@@ -7,15 +7,13 @@ var router = express.Router();
 // Get Books 
 
 router.get('/books', async function (req, res, next) {
-    try {
-        const books = await Book.findAll();
-        console.log(books);
-        res.render('index', {
-            books: books
-        })
-    } catch (error) {
-        next(error)
-    }
+    const books = await Book.findAll();
+    console.log(books);
+    res.render('index', {
+        books: books,
+        title: 'Books'
+    });
+
 })
 
 router.get('/books/new', async function (req, res, next) {
@@ -24,13 +22,9 @@ router.get('/books/new', async function (req, res, next) {
     });
 })
 
-router.post('/books/new', async function (req, res, next) {
-    try {
-        await Book.create(req.body);
-        res.redirect('/books')
-    } catch (error) {
-        next(error)
-    }
+router.post('books/new', async function (req, res, next) {
+    await Book.create(req.body)
+    res.redirect('/books')
 })
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+const Book = require('../models').Book
+
 var express = require('express');
 var router = express.Router();
 
